0d04d08bb6056279561e1ce3cc3edefb173ff697
Paul AF <66976703+ErpoowSoap@users.noreply.github.com>
2024-04-09 21:21:15 +0200

    Add files via upload

---
+++ app/src/main/java/com/example/a3and/NoInternetActivity.kt
@@ -0,0 +1,21 @@
+package com.example.a3and
+
+import android.app.Activity
+import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+
+class NoInternetActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nointernet)
+        val retryButton = findViewById<Button>(R.id.retryButton)
+        retryButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+
+}
